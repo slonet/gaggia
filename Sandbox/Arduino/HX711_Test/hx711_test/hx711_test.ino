5540c2c8f67098d1424d5d4dd85b8c1737400adc
Tyler Slone <tyler.slone47@gmail.com>
2023-11-13 11:20:14 -0800

    talking to the hx711 and reading back raw data

--- Sandbox/Arduino/HX711_Test/hx711_test/hx711_test.ino
+++ Sandbox/Arduino/HX711_Test/hx711_test/hx711_test.ino
@@ -1,26 +1,66 @@
-#include <Wire.h>
+uint8_t _clk_pin = 21;
+uint8_t _data_pin = 20;
+uint8_t _pulse_time_us = 1; // defaut to a clock speed of 1 MHz
+int32_t _raw_data = 0;
 
-void setup() {
-  Wire.begin();
+void setup() 
+{
   Serial.begin(115200);
 
+  pinMode(_data_pin, INPUT);
+  pinMode(_clk_pin, OUTPUT);
+  digitalWrite(_clk_pin, 0);
 }
 
-/*
-Waits for the HX711 to complete a measurement
-Sends clock pulses to the HX711 at the specified rate. 
-Sends the correct number of clock pulses for the specified mode
+int set_clock_period(int clock_rate_khz)
+{
+  if (clock_rate_khz > 1000) 
+    {
+      clock_rate_khz = 1000;
+    }
 
-mode: int 0,1,2
-  0 = Input A, Gain = 128
-  1 = Input B, Gain = 32
-  2 = Input A, Gain = 64
+  int pulse_time_us = 1000 / (2 * clock_rate_khz); // The positive and negative clock pulses are 1/2 a cycle
 
-clock_rate: int 10 - 500
-  clock rate in kHz
-*/
-void read_load_cell(mode, clock_rate)
+  return pulse_time_us;
+}
 
-void loop() {
 
+int32_t read_hx711(uint8_t pulse_time_us)
+{
+  uint8_t read_cycles = 24;
+  uint8_t config_cycles = 1;
+  int32_t data = 0;
+
+  while (read_cycles > 0)
+  {
+    digitalWrite(_clk_pin, 1);
+    delayMicroseconds(pulse_time_us);
+    digitalWrite(_clk_pin, 0);
+
+    data |= digitalRead(_data_pin) && 0x01;
+    data = data << 1;
+
+    //delayMicroseconds(pulse_time_us);
+    read_cycles--;
+  }
+
+  data = data >> 1; // remove the final bit shift so the LSB is in the right place
+
+  while (config_cycles > 0)
+  {
+    digitalWrite(_clk_pin, 1);
+    //delayMicroseconds(pulse_time_us);
+    digitalWrite(_clk_pin, 0);
+    //delayMicroseconds(pulse_time_us);
+    config_cycles--;
+  }
+
+  return data;
+}
+
+void loop()
+{
+  while(digitalRead(_data_pin)) {}
+  _raw_data = read_hx711(_pulse_time_us);
+  Serial.println(_raw_data);
 }
